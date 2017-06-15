59eb317d906a1b8f1a7726ca920355f93206a3fc
dcalabrese22 <dcalabrese22@gmail.com>
2017-06-15 16:10:55 -0400

    testing recyclerview

--- app/src/androidTest/java/com/example/dan/baking_app/OpenIngredientsDetailViewTest.java
+++ app/src/androidTest/java/com/example/dan/baking_app/OpenIngredientsDetailViewTest.java
@@ -1,5 +1,7 @@
 package com.example.dan.baking_app;
 
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -13,9 +15,6 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-/**
- * Created by dcalabrese on 6/14/2017.
- */
 
 @RunWith(AndroidJUnit4.class)
 public class OpenIngredientsDetailViewTest {
@@ -28,12 +27,22 @@ public class OpenIngredientsDetailViewTest {
     @Test
     public void scrollToRecipeCard() {
         onView(withId(R.id.recyclerview_recipe_list))
-                .perform(RecyclerViewActions.scrollToPosition(0));
+                .perform(RecyclerViewActions.scrollToPosition(4));
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @Test
     public void clickRecipeCard() {
         onView(withId(R.id.recyclerview_recipe_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+                .perform(RecyclerViewActions.actionOnItemAtPosition(4, recyclerClick()));
+    }
+
+    public static ViewAction recyclerClick() {
+        return ViewActions.click();
     }
 }
